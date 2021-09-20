803c5b56a447f70d800e60886d6253626e010631
Ryan Chang <ryuchang45@gmail.com>
2021-09-19 21:24:50 -0400

    creation et connexion compte

---
+++ GestionStage/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+using System;
+using GestionStage.Server.Data;
+using GestionStage.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GestionStage.Server.Areas.Identity.IdentityHostingStartup))]
+namespace GestionStage.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DefaultConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+            });
+        }
+    }
+}
